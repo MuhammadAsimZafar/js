96a0d77b09e8fb54ad4c2787ded4bac5d98d0fae
mustafavi11 <56079823+mustafavi11@users.noreply.github.com>
2023-03-12 22:06:13 +0000

    Converstion of dataType

---
+++ 01_basic/03_conversionOpreation.js
@@ -0,0 +1,31 @@
+let score = "asim"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN not a number
+// "ture" => 1; false=> 0
+// "string" => NaN
+
+
+let isLoggedIn = "hastish"
+
+let booleanIsLoggedin = Boolean(isLoggedIn)
+console.log(booleanIsLoggedin);
+
+// 1 => true; 0 => false
+// "" => false
+// "string"=> true
+
+
+let someNumber = 33
+
+let stringNubmber = String(someNumber)
+console.log(stringNubmber);
+console.log(typeof stringNubmber);
