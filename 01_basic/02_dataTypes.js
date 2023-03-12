323d745f7e68d578535be84f79e81c926264bc46
mustafavi11 <56079823+mustafavi11@users.noreply.github.com>
2023-03-12 21:39:20 +0000

    datatype

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") // wer are using nodejs, not browser
+
+console.log(3
+    +
+    3);  // code readability should be high
+
+console.log("name")
+
+/* 
+
+https://tc39.es/ecma262/
+https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
+*/
+
+let name = "hello world"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint 
+// string =>
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol=> unique
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
